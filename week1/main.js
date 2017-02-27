ce811c9f3fd14e1b1dd35b72162a95677f6dd6c2
Andrew James <ajames20@gmail.com>
2017-02-27 10:25:48 +0200

    week one complete

--- week1/main.js
+++ week1/main.js
@@ -1,33 +1,41 @@
-let jobList = [
-    'Junior frontend developer - HTML5/CSS3/React',
-    'Web developer intern',
-    'Full stack developer - Node.js/React',
-    'Senior backend developer - PHP/Node.js/mySQL',
-    'iOS developer',
-    'Android developer'
-];
-let jobTest = document.body.appendChild(document.createElement('div'));
-jobTest.setAttribute('id', 'jobTest');
-jobTest.innerHTML = 'Test';
+'use strict';
 
-function removeId() {
-    jobTest.remove();
-    addClass();
-};
+(function() {
+    function addClass() {
+        let newClass = document.body.appendChild(document.createElement('div'));
+        newClass.classList.add('card-layout');
+        let url = 'https://jobs.github.com/positions.json?description=html&css';
 
-function addClass() {
-    let newClass = document.body.appendChild(document.createElement('div'));
-    newClass.classList.add('list-job', 'container');
-    let list = document.createElement('ul')
-    newClass.appendChild(list);
+        fetch(url, {
+            method: 'get',
+            mode: 'cors'
 
-    for (var i = 0; i < jobList.length; i++) {
-        let item = document.createElement("li");
-        item.innerHTML = jobList[i];
-        list.appendChild(item);
+        }).then(function(resp) {
+            return resp.json();
+        }).then(function(data) {
+            console.log(data);
+            for (var i = 0; i < data.length; i++) {
+                let jobLink = data[i].url,
+                    companyName = data[i].company,
+                    jobDescriptionJson = data[i].description.slice(0, 200) + '... ',
+                    jobDescription = jobDescriptionJson.replace(/(<([^>]+)>)/ig, ""),
+                    howToApply = data[i].url,
+                    jobTitle = data[i].title,
+                    photo = data[i].company_logo;
+
+                newClass.innerHTML += '<div class="card description"><a href="jobDescription">' +
+                    '<h3 class="title">' + companyName + '</h3>' +
+                    '<span class="job-title">' + jobTitle + '</span>' +
+                    '<div class="description">' + jobDescription + '</div></a>';
+            }
+
+        }).catch(function(error) {
+            // If there is any error you will catch them here
+            console.log(error);
+        });
+
     }
 
-}
+    addClass();
 
-
-removeId();
+})();
