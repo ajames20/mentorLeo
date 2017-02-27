a8b4ab9f49cbf54dfa33a8cd032bc60d7bcbeb40
Andrew James <ajames20@gmail.com>
2017-02-27 10:32:42 +0200

    href edits

--- week1/main.js
+++ week1/main.js
@@ -13,17 +13,17 @@
         }).then(function(resp) {
             return resp.json();
         }).then(function(data) {
-            console.log(data);
+
             for (var i = 0; i < data.length; i++) {
                 let jobLink = data[i].url,
                     companyName = data[i].company,
                     jobDescriptionJson = data[i].description.slice(0, 200) + '... ',
                     jobDescription = jobDescriptionJson.replace(/(<([^>]+)>)/ig, ""),
-                    howToApply = data[i].url,
                     jobTitle = data[i].title,
                     photo = data[i].company_logo;
 
-                newClass.innerHTML += '<div class="card description"><a href="jobDescription">' +
+                console.log(jobLink);
+                newClass.innerHTML += '<div class="card description"><a href="' + jobLink + '" target="_blank">' +
                     '<h3 class="title">' + companyName + '</h3>' +
                     '<span class="job-title">' + jobTitle + '</span>' +
                     '<div class="description">' + jobDescription + '</div></a>';
